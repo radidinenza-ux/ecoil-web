19ee3aa1b2887e1f82096bd904cc06cbc1a01226
radidinenza-ux <radidinenza@gmail.com>
2026-05-24 15:47:25 +0700

    Update page.tsx

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -408,7 +408,7 @@ export default function AdminDashboardPage() {
                         </tr>
                       );
                     })()}
-                  </tbody> 
+                  </tbody>
       </div>
     </main>
   );
